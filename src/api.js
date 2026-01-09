7fed17201c76753a82c8ae5e539e5261ee1bf767
MatthewMonaghan <80243819+matthewjmon@users.noreply.github.com>
2026-01-09 12:43:11 +0200

    Update API base URL to production endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://scrapyard-register-app-backend.onrender.com/api",
 });
 
 // Attach JWT from localStorage to every request
